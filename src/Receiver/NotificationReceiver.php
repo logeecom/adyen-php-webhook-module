72e94efe16a5217b6bcc87318c377752a71c72ea
Aleksandar Sasa Boljanovic <aleksandar.boljanovic@logeecom.com>
2023-11-27 13:03:07 +0100

    Revert "Add compatability for HTTP Auth with PHP in CGI-mode"
    This reverts commit 9183fe59336dbc1d08428f6cbed78be14125236f.

--- src/Receiver/NotificationReceiver.php
+++ src/Receiver/NotificationReceiver.php
@@ -1,5 +1,4 @@
-<?php
-declare(strict_types=1);
+<?php declare(strict_types=1);
 /**
  *                       ######
  *                       ######
@@ -89,16 +88,8 @@ class NotificationReceiver
             }
             return false;
         }
-
-        $username = $_SERVER['PHP_AUTH_USER'];
-        $password = $_SERVER['PHP_AUTH_PW'];
-
-        if (!isset($username) || !isset($password)) {
-            list($username, $password) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
-        }
-
         // validate username and password
-        if (!isset($username) || !isset($password)) {
+        if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
             if ($isTestNotification) {
                 $message = 'Authentication failed: PHP_AUTH_USER or PHP_AUTH_PW are empty.';
                 throw new AuthenticationException($message);
@@ -106,8 +97,8 @@ class NotificationReceiver
             return false;
         }
 
-        $usernameIsValid = hash_equals($notificationUsername, $username);
-        $passwordIsValid = hash_equals($notificationPassword, $password);
+        $usernameIsValid = hash_equals($notificationUsername, $_SERVER['PHP_AUTH_USER']);
+        $passwordIsValid = hash_equals($notificationPassword, $_SERVER['PHP_AUTH_PW']);
         if ($usernameIsValid && $passwordIsValid) {
             return true;
         }
